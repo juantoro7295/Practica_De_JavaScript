9d239652e4badb643fa7003c4ecb18a0f1f26f4b
juantoro7295 <juanpatoro2011@gmail.com>
2022-04-24 20:48:34 -0500

    Práctica MVC parte 2
    se agregan las barras del juego pinpong y se corrige un error en index.html

--- main.js
+++ main.js
@@ -1,37 +1,77 @@
+
 (function () {
-    self.Board = function(width, height){
+    self.Board = function (width, height) {
         this.width = width;
         this.height = height;
         this.playing = false;
         this.game_over = false;
         this.bars = [];
         this.ball = null;
-    }
+    };
 
     self.Board.prototype = {
         get elements() {
-          let elements = this.bars;
-          elements.push(ball);
-          return elements;
+            let elements = this.bars;
+            elements.push(this.ball);
+            return elements;
         },
-      };
-    })();
+    };
+})();
 
-    (function (){
-        self.BoardView = function(canvas,board){
-            this.canvas = canvas;
-            this.canvas.width = board.width;
-            this.canvas.height = board.height;
-            this.board = board;
-            this.ctx = canvas.getContext("2d");
-        }
-    })();
+
+(function () {
+    self.Bar = function (x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    };
+
+    self.Bar.prototype = {
+        down: function () { },
+        up: function () { },
+    };
+})();
+
+(function () {
+    self.BoardView = function (canvas, board) {
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+    };
     
-    window.addEventListener("load", main);
 
-    function main() {
-        let board = new Board(800,400);
-        console.log(board);
-        let canvas = document.getElementById('canvas');
-        let board_view = new BoardView(canvas, board);
+    self.BoardView.prototype = {
+        draw: function () {
+            for (let i = this.board.elements.length - 1; i >= 0; i--) {
+                let el = this.board.elements[i];
+                draw(this.ctx, el);
+            }
+        },
+    };
+    function draw(ctx, element) {
+        if (element !== null && element.hasOwnProperty("kind")) {
+            switch (element.kind) {
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
     }
+})();
+
+window.addEventListener("load", main);
+
+function main() {
+    let board = new Board(800, 400);
+    let bar = new Bar(20, 100, 40, 100, board);
+    let bar2 = new Bar(735, 100, 40, 100, board);
+    let canvas = document.getElementById('canvas');
+    let board_view = new BoardView(canvas, board);
+    board_view.draw();
+}
